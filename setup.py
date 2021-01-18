4f1e5a48decad6a4552fabaa7cf4ba2dec8b09e9
Chun Ly <astro.chun@gmail.com>
2021-01-17 21:55:52 -0700

    Add setup.py and requirements.txt for packaging and installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fr:
+    requirements = fr.read().splitlines()
+
+setup(
+    name='github-stats-pages',
+    version='0.0.1',
+    packages=['tests', 'github_stats_pages'],
+    url='https://github.com/astrochun/github-stats-pages',
+    license='MIT',
+    author='Chun Ly',
+    author_email='astro.chun@gmail.com',
+    description='Retrieve statistics for a user\'s repositories and populate the information onto a GitHub static page',
+    long_description_content_type='text/markdown',
+    install_requires=requirements
+)
