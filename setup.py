758e4bb6c0604a85d05d84252f44197417eabc97
Chun Ly <astro.chun@gmail.com>
2021-02-01 21:35:19 -0700

    Update setup.py
     - Include long description
     - Add classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,15 @@ setup(
     author='Chun Ly',
     author_email='astro.chun@gmail.com',
     description='Retrieve statistics for a user\'s repositories and populate the information onto a GitHub static page',
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires=requirements
+    install_requires=requirements,
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
 )
