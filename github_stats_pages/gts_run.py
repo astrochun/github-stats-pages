1eaeec526598b1a90799cc892b18ca887bf3c891
Chun Ly <astro.chun@gmail.com>
2021-02-21 12:01:45 -0700

    Add save_csv keyword for gts_run

--- github_stats_pages/gts_run.py
+++ github_stats_pages/gts_run.py
@@ -2,14 +2,17 @@ import pandas as pd
 import os
 
 
-def run_each_repo(username, token, reponame):
-    os.system(f'gts {username}:{token} {reponame}')
+def run_each_repo(username, token, reponame, save_csv=True):
+    if save_csv:
+        os.system(f'gts {username}:{token} {reponame}')
+    else:
+        os.system(f'gts {username}:{token} {reponame} no_csv')
 
 
-def run_all_repos(username, token, csv_file):
+def run_all_repos(username, token, csv_file, save_csv=True):
     df = pd.read_csv(csv_file)
     # Exclude forks
     new_df = df.loc[df['fork'] == False]
 
     for reponame in new_df['name']:
-        run_each_repo(username, token, reponame)
+        run_each_repo(username, token, reponame, save_csv=save_csv)
