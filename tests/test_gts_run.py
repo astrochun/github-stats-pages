30e06fcdaed328060f52953cc205503398d712af
Chun Ly <astro.chun@gmail.com>
2021-03-08 21:05:28 -0700

    Clean up after run_each_repo unit test

--- tests/test_gts_run.py
+++ tests/test_gts_run.py
@@ -7,4 +7,7 @@ def test_run_each_repo(username, token):
     assert list(Path('.').glob('*stats.csv')) == []
 
     gts_run.run_each_repo(username, token, 'github-stats-pages', save_csv=True)
-    assert len(list(Path('.').glob('*stats.csv'))) == 3
+    p0 = Path('.').glob('*stats.csv')
+    assert len(list(p0)) == 3
+    for file in p0:
+        Path(file).unlink()
