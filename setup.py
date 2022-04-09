1f5a76046e0979eaa99a9809f68fefae95e602f4
Chun Ly <astro.chun@gmail.com>
2022-04-08 21:04:29 -0700

    Add docker-push job in docker.yml #74
     - Push to Docker Hub and Github Container registry
     - Add .dockerignore file
     - Rename docker YAML file
     - Bump version: v0.4.6 -> 0.4.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='github-stats-pages',
-    version='0.4.6',
+    version='0.4.7',
     packages=['github_stats_pages'],
     scripts=['scripts/get_repo_list',
              'scripts/gts_run_all_repos',
