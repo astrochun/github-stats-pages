6f4e1cfd2bcbe53c336587ef3983e0fa5738bb66
Chun Ly <astro.chun@gmail.com>
2022-05-01 16:30:21 -0700

    Add setup.cfg for metadata #76 (#78)
    - Add pyproject.toml for build requirements
     - Adjust Dockerfile for pip package install, add pyproject.toml and setup.cfg
     - Delete requirements.txt
     - Update .dockerignore
     - Bump version: v0.4.8 -> 0.4.9

--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding='utf-8') as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r") as fr:
-    requirements = fr.read().splitlines()
-
-setup(
-    name='github-stats-pages',
-    version='0.4.8',
-    packages=['github_stats_pages'],
-    scripts=['scripts/get_repo_list',
-             'scripts/gts_run_all_repos',
-             'scripts/merge-csv.sh',
-             'scripts/make_stats_plots'],
-    include_package_data=True,
-    package_data={'github_stats_pages': ['templates/*']},
-    url='https://github.com/astrochun/github-stats-pages',
-    license='MIT',
-    author='Chun Ly',
-    author_email='astro.chun@gmail.com',
-    description='Retrieve statistics for a user\'s repositories and populate the information onto a GitHub static page',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    install_requires=requirements,
-    python_requires='>=3.7',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
-    ]
-)
+setup()
